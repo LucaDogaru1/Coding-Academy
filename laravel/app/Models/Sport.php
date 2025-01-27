421b90ddbc15621d4b7ff8745f2bcac63bcf8062
l.dogaru <l.dogaru@sportradar.com>
2025-01-27 13:21:27 +0100

    adds the needed models

--- laravel/app/Models/Sport.php
+++ laravel/app/Models/Sport.php
@@ -2,22 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sport extends Model
 {
     protected $fillable = [
         'name',
-        'teams'
+        'home_team',
+        'away_team'
     ];
 
-    protected $casts = [
-        'teams' => 'array'
-    ];
 
-    public function calender(): BelongsTo
+    public function sportEvents(): HasMany
     {
-        return $this->belongsTo(Calender::class);
+        return $this->hasMany(SportEvent::class, 'sports_id');
     }
+
+    // Build in function offered by laravel, used for filtering,  called like this "->teams"
+        public function scopeTeam(Builder $query, $teamName): Builder
+    {
+        return $query->where('home_team', 'like', '%' . $teamName . '%')
+            ->orWhere('away_team', 'like', '%' . $teamName . '%');
+    }
+
 }
